2970ada4e3072304a87d9362e8c56da3bdb71d33
Mahesh Singh Shekhawat <msrzx7@gmail.com>
2025-05-30 11:31:45 +0530

    Update chapter 5 exercise 1.py

--- chapter-05-iterations/chapter 5 exercise 1.py
+++ chapter-05-iterations/chapter 5 exercise 1.py
@@ -1,5 +1,13 @@
-total = 0
-count = 0
+''' Exercise 1: Write a program which repeatedly reads integers until the user enters “done”. Once “done” is entered, print out the total, count, and average of the integers. 
+If the user enters anything other than a integers, detect their mistake using try and except and print an error message and skip to the next integers.
+
+Enter a number: 4
+Enter a number: 5
+Enter a number: bad data
+Invalid input
+Enter a number: 7
+Enter a number: done
+16 3 5.333333333333333''
 
 while True:
     num = input("Enter a number and done when you finished : ")
